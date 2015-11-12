a0266e5ffb734e74d098a5e79829f5ced9ae7628
jflores1 <jflores1@alumni.nd.edu>
2015-11-12 15:44:27 -0500

    installs deploy gem

--- config.rb
+++ config.rb
@@ -81,3 +81,13 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# Configure Deployment
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
